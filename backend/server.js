cf66c459a33877c10da9bc13dc5fae7dbe2ec9ad
Viraj Jadhav <virajssjadhav@gmail.com>
2020-10-21 01:51:14 +0530

    feat: basic db connection to mysql

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 const express = require("express")
+const mysql = require("mysql")
 const cors = require("cors")
 
 const app = express()
 
 const port = process.env.PORT || 5000
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    // database: "new_db"
+})
+
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected to database !");
+    connection.query("CREATE DATABASE IF NOT EXISTS ApartmentRent", function(err, res) {
+        if (err) throw err;
+        console.log("Database created");
+    })
+})
 
 app.use(express.json({ limit: "50mb" }))
 app.use(cors())
