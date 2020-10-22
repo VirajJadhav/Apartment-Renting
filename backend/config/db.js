bd4a570eff2e09bd1a33bf0e3e78fe4eeeb686a1
Viraj Jadhav <virajssjadhav@gmail.com>
2020-10-22 18:09:32 +0530

    feat: create table queries for whole database schema

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,12 @@
 const mysql = require("mysql")
 
+const tables = require("./table");
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
+    database: "ApartmentRent",
     multipleStatements: true
 })
 
@@ -17,5 +20,34 @@ connection.query("CREATE DATABASE IF NOT EXISTS ApartmentRent", function(error, 
     console.log("Database created !");
 })
 
+connection.query(tables.user, function(error, result) {
+    if(error) throw error;
+    console.log("User table created !");
+})
+
+connection.query(tables.owner, function(error, result) {
+    if(error) throw error;
+    console.log("Owner table created !");
+})
+
+connection.query(tables.tenant, function(error, result) {
+    if(error) throw error;
+    console.log("Tenant table created !");
+})
+
+connection.query(tables.building, function(error, result) {
+    if(error) throw error;
+    console.log("Building table created !");
+})
+
+connection.query(tables.flats, function(error, result) {
+    if(error) throw error;
+    console.log("Flats table created !");
+})
+
+connection.query(tables.payment, function(error, result) {
+    if(error) throw error;
+    console.log("Payment table created !");
+})
 
 module.exports = connection;
---
+++ backend/config/table.js
@@ -0,0 +1,10 @@
+const tables = {
+    "user": "CREATE TABLE IF NOT EXISTS user ( user_email varchar(255) NOT NULL, user_password varchar(255), user_type char(1), primary key (user_email) )",
+    "owner": "CREATE TABLE IF NOT EXISTS owner ( owner_email varchar(255) NOT NULL, owner_name varchar(25), owner_contact varchar(10), owner_street varchar(255), owner_pincode varchar(6), PRIMARY KEY (owner_email), FOREIGN KEY (owner_email) REFERENCES user(user_email) ON DELETE CASCADE )",
+    "tenant": "CREATE TABLE IF NOT EXISTS tenant ( tenant_email varchar(255) NOT NULL, tenant_name varchar(25), tenant_contact varchar(10), tenant_street varchar(255), tenant_pincode varchar(6), PRIMARY KEY (tenant_email), FOREIGN KEY (tenant_email) REFERENCES user(user_email) ON DELETE CASCADE )",
+    "building": "CREATE TABLE IF NOT EXISTS building ( buildingID int AUTO_INCREMENT NOT NULL, building_name varchar(255), total_floors int, flats_each int, building_street varchar(255), building_pincode varchar(6), PRIMARY KEY (buildingID) )",
+    "flats": "CREATE TABLE IF NOT EXISTS flats ( flatID int AUTO_INCREMENT NOT NULL, owner_email varchar(255), tenant_email varchar(255), buildingID int, floor_number int, flat_number int, start_date date, PRIMARY KEY (flatID), FOREIGN KEY (owner_email) REFERENCES owner(owner_email) ON DELETE CASCADE, FOREIGN KEY (tenant_email) REFERENCES tenant(tenant_email) ON DELETE CASCADE, FOREIGN KEY (buildingID) REFERENCES building(buildingID) ON DELETE CASCADE )",
+    "payment": "CREATE TABLE IF NOT EXISTS payment ( paymentID int AUTO_INCREMENT NOT NULL, flatID int, buildingID int, paid char(1), due_date date, rent_amount int, PRIMARY KEY (paymentID), FOREIGN KEY (flatID) REFERENCES flats(flatID) ON DELETE CASCADE, FOREIGN KEY (buildingID) REFERENCES building(buildingID) ON DELETE CASCADE )"
+}
+
+module.exports = tables;
